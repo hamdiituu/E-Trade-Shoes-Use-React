9bcf73142d29f1110e5aa8c985e844208275a293
Hamdi Tuğ <hamditug@apple-MacBook.local>
2020-07-12 21:46:27 +0300

    Updates

---
+++ precache-manifest.9e66a00256788b1e07324e10d3fbfbde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e460febfa12f5fae3be44690d7bea722",
+    "url": "/E-Trade-Shoes-Use-React/index.html"
+  },
+  {
+    "revision": "47e16af6dceeecc73651",
+    "url": "/E-Trade-Shoes-Use-React/static/css/main.d0a4c115.chunk.css"
+  },
+  {
+    "revision": "dcc06f82a63ed9ebd893",
+    "url": "/E-Trade-Shoes-Use-React/static/js/2.cfc03931.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/E-Trade-Shoes-Use-React/static/js/2.cfc03931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e16af6dceeecc73651",
+    "url": "/E-Trade-Shoes-Use-React/static/js/main.4d6ecff2.chunk.js"
+  },
+  {
+    "revision": "1833ae667a507ed3e59f",
+    "url": "/E-Trade-Shoes-Use-React/static/js/runtime-main.e24e69f8.js"
+  }
+]);
